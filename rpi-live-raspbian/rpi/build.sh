af459f760eeaddc73666b754a01e3abc6218e36f
elesos <hnrayer@gmail.com>
2019-08-29 16:34:04 +0800

    add ssl libs

---
+++ rpi-live-raspbian/rpi/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+this_path=$(cd `dirname $0`;pwd)  # $(pwd)
+echo "pwd=${this_path}"
+
+
+
+
+cd ${this_path}/../third/curl/lib
+ln -s libcurl.so.4.5.0 libcurl.so
+ln -s libcurl.so.4.5.0 libcurl.so.4
+
+ln -s libcrypto.so.3 libcrypto.so
+ln -s libssl.so.3 libssl.so
+
+make clean && make 
+echo "success"
